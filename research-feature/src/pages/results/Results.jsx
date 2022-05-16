a8d203100b3c7a2664f6f39c262fb98ae5b5e6b4
Leo Rigotto <leorigotto@me.com>
2022-05-16 00:17:52 -0300

    [feat] results page fetches data and shows as charts

--- research-feature/src/pages/results/Results.jsx
+++ research-feature/src/pages/results/Results.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import Loading from "../../assets/loading/Loading"
+import Graphs from "../../components/Graphs"
+import { BASE_URL } from "../../services/BASE_URL"
 
 export default function Results() {
+  const [answer1, setAnswer1] = useState()
+  const [answer2, setAnswer2] = useState()
+
+  let chartData1
+  if (answer1) {
+    chartData1 = Object.keys(answer1).map((item) => {
+      return {
+        id: item,
+        label: item,
+        value: answer1[item],
+      }
+    })
+  }
+  let chartData2
+  if (answer2) {
+    chartData2 = Object.keys(answer2).map((item) => {
+      return {
+        id: item,
+        label: item,
+        value: answer2[item],
+      }
+    })
+  }
+
+  // this could be better
+  const getAnswer1 = () => {
+    axios
+      .get(`${BASE_URL}/answer/1`)
+      .then((res) => {
+        setAnswer1(res.data)
+      })
+      .catch((err) => console.log(err.response))
+  }
+  const getAnswer2 = () => {
+    axios
+      .get(`${BASE_URL}/answer/2`)
+      .then((res) => {
+        setAnswer2(res.data)
+      })
+      .catch((err) => console.log(err.response))
+  }
+  useEffect(() => {
+    getAnswer1()
+    getAnswer2()
+  }, [])
+
   return (
-    <div>Results</div>
+    <div style={{ height: "100vh" }}>
+      {chartData1 ? <Graphs data={chartData1} /> : <Loading />}
+      {chartData2 ? <Graphs data={chartData2} /> : <Loading />}
+    </div>
   )
 }
--- research-feature/src/services/fetchResults.js
+++ research-feature/src/services/fetchResults.js
@@ -1,3 +1,27 @@
+import axios from "axios"
+import { BASE_URL } from "./BASE_URL"
 
+export function getAnswer1() {
+  axios
+    .get(`${BASE_URL}/answer/1`)
+    .then((res) => {
+      return res.data
+    })
+    .catch((err) => console.log(err.response))
+}
 
-export async function fetchResults
+export function getAnswer2() {
+  axios
+    .get(`${BASE_URL}/answer/2`)
+    .then((res) => {
+      return res.data
+    })
+    .catch((err) => console.log(err.response))
+}
+
+export function fetchResults() {
+  try {
+    let results = { answer1: getAnswer1(), answer2: getAnswer2() }
+    return results
+  } catch (error) {}
+}
